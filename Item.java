fc52a519cef29c0c805472c7967836cf669e6dbb
nhjia <1462267442@qq.com>
2020-10-08 17:22:38 +0800

    item

---
+++ Item.java
@@ -0,0 +1,19 @@
+
+public class Item {
+	String name;
+	int price;
+	public static void main(String[] args) {
+		Item xueping = new Item();
+		xueping.name="ÑªÆ¿";
+		xueping.price=50;
+		
+		Item caoxie = new Item();
+		caoxie.name="²ÝÐ¬";
+		caoxie.price=300;
+		
+		Item changjian = new Item();
+		changjian.name="²ÝÐ¬";
+		changjian.price=300;
+	}
+
+}

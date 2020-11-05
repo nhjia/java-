63bf331085f985141fd72f088f8f2cad47ec2fa4
nhjia <1462267442@qq.com>
2020-11-05 21:57:46 +0800

    Spring AOP

---
+++ SpringAOP/src/nhjia/test/TestSpring.java
@@ -0,0 +1,18 @@
+package nhjia.test;
+
+import nhjia.pojo.Project;
+import com.nhjia.service.ProductService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext(
+                new String[] { "applicationContext.xml" });
+        ProductService s = (ProductService) context.getBean("s");
+        s.doSomeService();
+
+
+    }
+}
---
+++ SpringTest/src/com/nhjia/pojo/Project.java
@@ -0,0 +1,4 @@
+package com.nhjia.pojo;
+
+public class Project {
+}
